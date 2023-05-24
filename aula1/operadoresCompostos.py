01be4748c3d8903751277b6f715f4a08e9d3b7dc
natancarrapeiro <natancarrapeiro@gmail.com>
2023-05-24 09:53:48 -0300

    variaveis

---
+++ aula1/operadoresCompostos.py
@@ -0,0 +1,17 @@
+x,y,a,b,u=2,4,5,50,1245
+#Mais igual
+x +=2 #Equivale x=x+2
+#Menos igual
+y -=2#equivale y =y-2
+#Vezes igual
+a *=2 #Equivale a=a*2
+#Dividido igual
+b /=2#equivale b=b/2
+#Módulo igual (Ele retorna o resto da divisão do operando da esquerda pelo operando da direita.)
+u %=3.5#Equivale u=u%2
+print(x)
+print(y)
+print(a)
+print(b)
+print(u)
+
---
+++ aula1/variaveis.py
@@ -0,0 +1,8 @@
+x=22#variavel int
+y="natan" #variavel string
+idade=x#variavel que contem outra variavel
+#outra forma de criar variavel
+a,d,f,g=2,5,4,1#a=2 d=5 f=4 g=1
+print (f"o aluno {y} tem {idade} anos de idade")
+#ao utilizar  texto com variavel se utiliza o F"texto{variavel desejada}"
+#assim vc condegue montar palavras e variaveis 

b70ea0ae4d1fa23a5c9f635b8b1dae45fd1d4a65
natancarrapeiro <natancarrapeiro@gmail.com>
2023-05-24 13:01:09 -0300

    listas_Dentro_de_listas

---
+++ aula1/listaDentro_DaLista.py
@@ -0,0 +1,12 @@
+par=[2,4,6,8,10]
+impar=[1,3,5,7,9,11]
+cores=["amarelo","red","azul"]
+listas=[par,impar,cores]
+print(par)
+print()
+print(impar)
+print()
+print(cores)
+print()
+print(listas)
+print (f'lista [1] dentro da lista (listas){listas[1]}')

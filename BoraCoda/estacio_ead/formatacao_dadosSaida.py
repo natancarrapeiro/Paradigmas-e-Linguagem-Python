dbf260450a736a823a3ea73ea642b6f9cb3ce10b
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-07-11 10:10:42 -0300

    codigo testes

---
+++ BoraCoda/estacio_ead/formatacao_dadosSaida.py
@@ -0,0 +1,13 @@
+hora=10
+minutos=25
+segundos=17
+print(f'{str(hora)} : {str(minutos)} : {str(segundos)}')
+hora2=16
+minutos2=25
+segundos2=36
+print('{} : {} : {}'.format(hora2,minutos2,segundos2))
+
+print('{:8.5}'.format(10/3))
+#Ao usar {:8.5}, estamos determinando que a impressão será com 8 espaços, mas apenas 5 serão utilizados.
+L = [1,2,3,4,5,6,7,8]
+print (L[::-1])

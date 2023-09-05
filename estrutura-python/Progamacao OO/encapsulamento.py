411b7dfd42f2eaa04abfc6242b85ea3077f54a26
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-09-05 18:44:46 -0300

    encapsulamento

---
+++ estrutura-python/Progamacao OO/encapsulamento.py
@@ -0,0 +1,24 @@
+class Conta:
+    def __init__(self, numero, saldo):
+     self.__numero = numero
+     self.__saldo = saldo
+
+    def depositar(self,valorDeposito):
+      if( valorDeposito < self.__saldo):
+        self.__saldo +=valorDeposito
+        print("transferencia feita")
+        print(self.__saldo)
+        return 
+      else:
+        print("saldo inferior")  
+    
+    def pega_saldo(self):
+        return self.__saldo
+    
+
+conta = Conta(1, 1000)
+# print(conta.__saldo=500) #atributo privado
+conta.depositar(300)
+
+conta.pega_saldo()
+    

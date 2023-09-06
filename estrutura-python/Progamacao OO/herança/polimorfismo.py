f9c44f98e4370c61d22a64b705bfe5fabe8f89f8
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-09-06 17:30:44 -0300

    polimorfismo

---
+++ estrutura-python/Progamacao OO/herança/polimorfismo.py
@@ -0,0 +1,26 @@
+class A:
+    def __init__(self,nome) :
+        self.nome=nome
+    
+    def ola(self):
+        pass# significa que sera definido o que ele faz pelo filho
+                    
+
+class B(A):
+    def __init__(self, nome):
+        super().__init__(nome)
+    def ola(self):
+        print(self.nome) 
+class C(A):
+    def __init__(self, nome,sobrenome):
+        self.sobrenome=sobrenome
+        super().__init__(nome)
+    def ola(self):
+        print(self.nome,self.sobrenome) 
+
+
+d=B("natan")
+d.ola()   
+
+e=C("natan","carrapeiro")
+e.ola()
